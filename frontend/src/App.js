5155112fb67fe8e8a0bf5a25836bbc8da29ba93b
Caidler <rafal.szalkiewicz@gmail.com>
2020-06-30 20:14:25 +0200

    added auto-dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,33 @@ import axios from "axios";
 import Login from "./components/Auth/Login.component";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage.component";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   // store users in a new variable
   axios.defaults.xsrfHeaderName = "X-CSRFToken";
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
 
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={HomePage} />{" "}
-        <Route exact path="/login/" component={Login} />{" "}
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Switch>
+          <Route exact path="/" component={HomePage} />{" "}
+          <Route exact path="/login/" component={Login} />{" "}
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
