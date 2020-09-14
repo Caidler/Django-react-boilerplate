b2d1663c7a3dc86d357496e5d4a554d7ca4a3563
Caidler <rafal.szalkiewicz@gmail.com>
2020-09-14 22:08:08 +0200

    monkey patched header + logo update

--- frontend/src/components/Header/Header.component.jsx
+++ frontend/src/components/Header/Header.component.jsx
@@ -10,12 +10,13 @@ import Button from "@material-ui/core/Button";
 import DefaultSnackbar from "../Snackbar/Snackbar.component";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import IconButton from "@material-ui/core/IconButton";
-import Logo from "../../assets/logo.svg";
+import Logo from "../../assets/logo.png";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import React from "react";
 import Switch from "@material-ui/core/Switch";
 import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import { alert } from "../../store/actions/";
 import { logout } from "../../store/actions/";
 import { makeStyles } from "@material-ui/core/styles";
@@ -24,8 +25,11 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuSwitch: {
-    marginRight: theme.spacing(2),
+  // menuSwitch: {
+  //   marginRight: theme.spacing(2),
+  //   flexGrow: 1,
+  // },
+  title: {
     flexGrow: 1,
   },
 }));
@@ -67,6 +71,7 @@ function MenuAppBar(props) {
               <img src={Logo} alt="Logo" />
             </div>
           </Link>
+          <Typography variant="h6" className={classes.title}></Typography>
           <FormControlLabel
             className={classes.menuSwitch}
             value="start"
